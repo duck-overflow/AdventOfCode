3a7573fff0294ee56f7389f9f06f2a9d7d195f13
DuckDestroyer <thorbenlestin@icloud.com>
2023-12-02 12:17:29 +0100

    - solved day 2 in python

--- src/de/duckdestroyer/aoc/main/Main.java
+++ src/de/duckdestroyer/aoc/main/Main.java
@@ -1,16 +1,9 @@
 package de.duckdestroyer.aoc.main;
 
-import de.duckdestroyer.aoc.dayOne.taskOne;
-import de.duckdestroyer.aoc.dayOne.taskTwo;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        taskOne tO = new taskOne();
-        taskTwo tT = new taskTwo();
-        tO.importTxt();
-        tT.taskTwo();
 
     }
 }
