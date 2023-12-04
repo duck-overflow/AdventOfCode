ed111f3892ac552401d29252dde856ae2d7a97f5
duckdestroyer <146432133+DuckDestroyer@users.noreply.github.com>
2023-12-04 08:54:19 +0100

    Day Three - solved

--- src/de/duckdestroyer/aoc/main/Main.java
+++ src/de/duckdestroyer/aoc/main/Main.java
@@ -1,6 +1,5 @@
 package de.duckdestroyer.aoc.main;
 
-import de.duckdestroyer.aoc.dayFour.taskOne;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
